f319fc7c5a2514a311d2aad9d9c3bab52eab2a38
jbboisnault <50906878+jbboisnault@users.noreply.github.com>
2026-06-23 17:59:25 +0200

    Update supabase.js

--- js/supabase.js
+++ js/supabase.js
@@ -1,5 +1,5 @@
 // js/supabase.js
-const SUPABASE_URL = 'https://TON-PROJET.supabase.co';
+const SUPABASE_URL = 'https://cqysptinibknqiztnpoc.supabase.co/rest/v1/';
 const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImNxeXNwdGluaWJrbnFpenRucG9jIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODIyMTk5NTMsImV4cCI6MjA5Nzc5NTk1M30.ooMQXLq_QUFEw71pNjW3lbVvaZEn-1Q1w92PRbsKuIs';
 
 const supabaseClient = supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
